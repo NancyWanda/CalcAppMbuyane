2f5813d74d80574d51a2b9e019203cb4a9f987dd
NancyWanda <68513010+NancyWanda@users.noreply.github.com>
2021-05-09 13:11:41 +0200

    Update CalcAppMbuyane.java

--- src/calcappmbuyane/CalcAppMbuyane.java
+++ src/calcappmbuyane/CalcAppMbuyane.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package calcappmbuyane;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +13,27 @@ public class CalcAppMbuyane {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    
+    int a, b, add, subtract, multiply;
+    float divide; 
+     
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter Two Numbers : ");
+    a = scanner.nextInt();
+    b = scanner.nextInt();
+
+    add = a + b;
+    subtract = a - b;
+    multiply = a * b;
+    devide = (float) a / b;
+
+    System.out.println("Sum = " + add);
+    System.out.println("Difference = " + subtract);
+    System.out.println("Multiplication = " + multiply);
+    System.out.println("Division = " + divide);
+        
     }
     
 }
